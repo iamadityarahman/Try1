e791c1d26a56dc6007ff45623488e084333cd5fe
Aditya Rahman <adityarahman032@gmail.com>
2017-12-25 22:50:40 +0700

    Merubah Dekalarasi Variabel
    Variabel diubah menjadi array 1 dimensi dan multidimensi

--- SAW.cpp
+++ SAW.cpp
@@ -3,52 +3,23 @@
 using namespace std;
 
 int main(){
-	// DEKLARASI VARIABEL
-	double a1c1,a1c2,a1c3,a1c4,a1c5;
-	double a2c1,a2c2,a2c3,a2c4,a2c5;
-	double a3c1,a3c2,a3c3,a3c4,a3c5;
-
-	double Ra1c1,Ra1c2,Ra1c3,Ra1c4,Ra1c5;
-	double Ra2c1,Ra2c2,Ra2c3,Ra2c4,Ra2c5;
-	double Ra3c1,Ra3c2,Ra3c3,Ra3c4,Ra3c5;
-
-	double RWa1c1,RWa1c2,RWa1c3,RWa1c4,RWa1c5;
-	double RWa2c1,RWa2c2,RWa2c3,RWa2c4,RWa2c5;
-	double RWa3c1,RWa3c2,RWa3c3,RWa3c4,RWa3c5;
-
-	double w1,w2,w3,w4,w5;
-	double mc1,mc2,mc3,mc4,mc5;
-	double ha1,ha2,ha3;
+	// DEKLARASI VARIABEL, ARRAY DAN ISI DATA
+	int x,y;
+	double 	arrayM[5],
+			arrayH[3],
+			matrixR[3][5],
+			matrixRxW[3][5],
+			arrayW[5] = {5,3,4,4,2},
+			array[3][5] = {
+					{4,4,5,3,3},
+					{3,3,4,2,3},
+					{5,4,2,2,2}
+			};
 
 	// SETTING PRESISI DESIMAL
 	cout.setf(ios::fixed);
 	cout.setf(ios::showpoint);
 	cout.precision(4);
-
-	// ISI DATA
-	w1=5;
-	w2=3;
-	w3=4;
-	w4=4;
-	w5=2;
-
-	a1c1=4;
-	a1c2=4;
-	a1c3=5;
-	a1c4=3;
-	a1c5=3;
-
-	a2c1=3;
-	a2c2=3;
-	a2c3=4;
-	a2c4=2;
-	a2c5=3;
-
-	a3c1=5;
-	a3c2=4;
-	a3c3=2;
-	a3c4=2;
-	a3c5=2;
 
 	// AMBIL NILAI MAX TIAP KRITERIA
 	mc1=max(a1c1,max(a2c1,a3c1));
